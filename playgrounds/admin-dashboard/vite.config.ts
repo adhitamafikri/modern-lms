9273a85ebe66fe8945afbe6b46a1944c1892f7f1
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2025-12-05 23:35:22 +0700

    chore: run platform and admin-dashboard locally with docker

--- playgrounds/admin-dashboard/vite.config.ts
+++ playgrounds/admin-dashboard/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // need to configure this to enable run with Docker locally
+  server: {
+    host: 'localhost',
+    port: 5173,
+  },
 })
---
+++ playgrounds/platform/app/auth/page.tsx
@@ -0,0 +1,3 @@
+export default function Auth() {
+  return <h1>This is auth</h1>;
+}
